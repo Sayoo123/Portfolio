ef8c0586f985422b3f01c9e0047b7b9f9f2334f1
Sayoo123 <sayoojsunil2000@gmail.com>
2023-01-27 14:29:54 +0000

    project-update

--- portfolio/script.js
+++ portfolio/script.js
@@ -2,3 +2,9 @@ function show(){
     document.querySelector('.ham').classList.toggle('open')
     document.querySelector('.navigation').classList.toggle('active')
 }
+document.addEventListener('DOMContentLoaded', function() {
+    console.log(document.getElementById('box1').getBoundingClientRect());
+   
+}, false);
+
+
